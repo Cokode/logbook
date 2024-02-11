0aaaf5a233d8d0c223f18d02386b7eb49de29c17
Collins C. A <contactmrc4@gmail.com>
2024-02-11 10:39:40 +0100

    removed numeric test

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -1,13 +1,9 @@
 import { assert } from "chai";
-import {it, describe} from "mocha"
+import {it, describe} from "mocha";
 
  describe('Global Tests', function() {
     it('page has a valid title', function() {
       assert(document.title && document.title.match(/\S/) && 
         document.title.toUpperCase() !== 'TODO');
-    });
-
-    it("should add two numbers correctly", function() {
-      assert(4 * 3, 12);
     });
   });
