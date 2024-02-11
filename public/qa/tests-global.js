fba5412665e4a135fe1a3b7d8289657e3a8106bd
Collins C. A <contactmrc4@gmail.com>
2024-02-11 11:22:56 +0100

    no major changes made

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -1,8 +1,5 @@
-import { assert } from "chai";
-import {it, describe} from "mocha";
-
- describe('Global Tests', function() {
-    it('page has a valid title', function() {
+suite('Global Tests', function() {
+    test('page has a valid title', function() {
       assert(document.title && document.title.match(/\S/) && 
         document.title.toUpperCase() !== 'TODO');
     });
