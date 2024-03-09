bad7acde4b35c70f535acc76e5376b28ea6abc7d
Collins C. A <contactmrc4@gmail.com>
2024-03-09 22:56:01 +0100

    Added 'home-layout' to res.render

--- routes/forgotPass.mjs
+++ routes/forgotPass.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/forgot-password', (req, res) => {
-  res.render('forgot-password', { layout: 'main'});
+  res.render('forgot-password', { layout: 'home-layout'});
 });
 
 export default router;
