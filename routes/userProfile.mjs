d3f2e440c218db5d2af3d17eb9a52968da93bc1a
Collins C. A <contactmrc4@gmail.com>
2024-05-18 07:54:10 +0200

    Removed unecessary code block -removed line of code were used for testing purpose only

--- routes/userProfile.mjs
+++ routes/userProfile.mjs
@@ -7,18 +7,6 @@ router.get('/user-profile', (req, res) => {
     res.redirect(303, '/');
   }
 
-  if(!req.session.editProfile) {
-    console.log('editProfile is set to false');
-    res.locals.editProfile = false;
-  } else {
-    res.locals.editProfile = true;
-    console.log('editProfile is set to true');
-    delete req.session.editProfile;
-    console.log('editProfile is deleted from session.');
-  }
-
-  console.log(res.locals.editProfile);
-
   const user = req.session.user;
   res.render('profile', {layout: 'main', user});
 });
